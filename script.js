50a2897966c8400b5f60995102ce7c1bbe16cb6e
Lincoln Kubasu <kubasulincoln@gmail.com>
2025-10-17 11:17:17 +0300

    script.js import

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
+import { showSlide, nextSlide, prevSlide, startAutoSlide } from "./slide.js"
+
+let slides = document.getElementsByClassName('slide')
+let dots = document.getElementsByClassName('dot')
+
 
